73055d8812395309621c22a543e048e79e06dcdc
Denzil Jude Daniels <denziljudedaniels@gmail.com>
2023-11-20 18:20:45 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
+import { Card } from './Card';
 import './App.css';
+import { RxHamburgerMenu } from "react-icons/rx";
+import { RiArrowDropDownLine } from "react-icons/ri";
+import OutsideAlerter from './ToggleDrop';
+import { useEffect, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [tickets, setTickets] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [group, setGroup] = useState('status');
+
+  useEffect(() => {
+    const llurl = '';
+    fetch(llurl)
+      .then(response => response.json())
+      .then(result => {
+        setTickets(() => {
+          let arr = result.tickets.map(d => d);
+          return arr;
+        });
+        setUsers(() => {
+          let arr = result.users.map(d => d);
+          return arr;
+        });
+      })
+      .catch(error => console.log('error', error));
+  }, []);
+
+  const [display, setDisplay] = useState('none');
+
+  const toggleDisplay = () => {
+    if (display === '')
+      setDisplay('none');
+  }
+
+  const changeGroup = (e) => {
+    if (e.target.value === "Status")
+      setGroup('status');
+    else if (e.target.value === "User")
+      setGroup('user');
+    else if (e.target.value === "Priority")
+      setGroup('priority');
+  }
+
+  const changeOrder = (e)=>{
+    if(e.target.value==="Priority")
+      sortByPriority();
+    else if(e.target.value==="Title")
+      sortByTitle();
+  }
+  const sortByPriority=()=>{
+  }
+
+  const sortByTitle=()=>{
+  }
+
+  return <div className="App">
+    <div className='dropdown'>
+      <OutsideAlerter display={display} toggleDisplay={toggleDisplay}>
+        <div className='app-display' onClick={() => setDisplay(d => {
+          if (d === '')
+            return 'none';
+          return '';
+        })}>
+          <RxHamburgerMenu size='2rem' />Display<RiArrowDropDownLine size='2rem' />
+        </div>
+        <div style={{ display: display }} className='app-dropdown-content'>
+          <div className='app-dropdown-group'>
+            <p>Grouping</p>
+            <select onChange={changeGroup}>
+              <option>Status</option>
+              <option>User</option>
+              <option>Priority</option>
+            </select>
+          </div>
+          <div className='app-dropdown-order'>
+            <p>Ordering</p>
+            <select onChange={changeOrder}>
+              <option>Priority</option>
+              <option>Title</option>
+            </select>
+          </div>
+        </div>
+      </OutsideAlerter>
     </div>
-  );
+    {group === 'status' ? <div>
+      {tickets.map(d => <Card id={d.id} title={d.title} tag={d.tag} userId={d.userId} status={d.status} priority={d.priority} />)}
+    </div>
+      : group === 'user' ? <div>
+        {users.map(d => <Card id={d.id} title={d.title} tag={d.tag} userId={d.userId} status={d.status} priority={d.priority} />)}
+      </div>
+        : group === 'priority' ? <div>
+          {tickets.map(d => <Card id={d.id} title={d.title} tag={d.tag} userId={d.userId} status={d.status} priority={d.priority} />)}
+        </div> : ''}
+  </div>
 }
 
 export default App;
---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import './Card.css';
+
+export const Card = ({ id, title, tag, userId, status, priority }) => {
+    return <div className='card'>
+        <div className='card-id-group'>
+            <h2 className='card-id'>{id}</h2>
+            <h2 className='card-user-id'>{userId}</h2>
+        </div>
+        <div className='card-title-group'>
+            {/* {status} */}
+            <h2 className='card-title'>{title}</h2>
+        </div>
+        <div className='card-info-group'>
+            {/* <div className='tag'>{priority}</div> */}
+            {tag?.map(t=><div className='tag'>{t}</div>)}
+        </div>
+    </div>
+}
